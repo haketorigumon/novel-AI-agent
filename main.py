37402a264af745b7ab1cbd19ab23beed3baa2f88
haketorigumon <minamian00@proton.me>
2025-09-28 17:33:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
 
-with open(sys.argv[1], "r", encoding="utf-8") as f:
-    data = f.read()
-chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
-print(f"data has {data_size} characters, {vocab_size} unique.")
-char_to_ix = { ch:i for i,ch in enumerate(chars) }
-ix_to_char = { i:ch for i,ch in enumerate(chars) }
-
-# hyperparameters
-hidden_size = 100 # size of hidden layer of neurons
-seq_length = 25 # number of steps to unroll the RNN for
-learning_rate = 1e-1
-
-# model parameters
-Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
-Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
-bh = np.zeros((hidden_size, 1)) # hidden bias
-by = np.zeros((vocab_size, 1)) # output bias
-
 def lossFun(inputs, targets, hprev):
   """
   inputs,targets are both list of integers.
@@ -75,35 +55,44 @@ def sample(h, seed_ix, n):
     ixes.append(ix)
   return ixes
 
-n, p = 0, 0
-mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
-smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
-while True:
-  # prepare inputs (we're sweeping from left to right in steps seq_length long)
-  if p+seq_length+1 >= len(data) or n == 0: 
-    hprev = np.zeros((hidden_size,1)) # reset RNN memory
-    p = 0 # go from start of data
-  inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-  targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
-
-  # sample from the model now and then
-  if n % 100 == 0:
-    sample_ix = sample(hprev, inputs[0], 200)
-    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-    print '----\n %s \n----' % (txt, )
-
-  # forward seq_length characters through the net and fetch gradient
-  loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
-  smooth_loss = smooth_loss * 0.999 + loss * 0.001
-  if n % 100 == 0: print 'iter %d, loss: %f' % (n, smooth_loss) # print progress
-  
-  # perform parameter update with Adagrad
-  for param, dparam, mem in zip([Wxh, Whh, Why, bh, by], 
-                                [dWxh, dWhh, dWhy, dbh, dby], 
-                                [mWxh, mWhh, mWhy, mbh, mby]):
-    mem += dparam * dparam
-    param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
-
-  p += seq_length # move data pointer
-  n += 1 # iteration counter 
+def main():
+    
+    with open(sys.argv[1], "r", encoding="utf-8") as f:
+        data = f.read()
+    chars = list(set(data))
+    data_size, vocab_size = len(data), len(chars)
+    print(f"data has {data_size} characters, {vocab_size} unique.")
+    char_to_ix = { ch:i for i,ch in enumerate(chars) }
+    ix_to_char = { i:ch for i,ch in enumerate(chars) }
+    hidden_size = 100
+    seq_length = 25
+    learning_rate = 1e-1
+    Wxh = np.random.randn(hidden_size, vocab_size)*0.01
+    Whh = np.random.randn(hidden_size, hidden_size)*0.01
+    Why = np.random.randn(vocab_size, hidden_size)*0.01
+    bh = np.zeros((hidden_size, 1))
+    by = np.zeros((vocab_size, 1))
+    n, p = 0, 0
+    mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
+    mbh, mby = np.zeros_like(bh), np.zeros_like(by)
+    smooth_loss = -np.log(1.0/vocab_size)*seq_length
+    while True:
+        if p+seq_length+1 >= len(data) or n == 0:
+            hprev = np.zeros((hidden_size,1))
+            p = 0
+        inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+        targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+        if n % 100 == 0:
+            sample_ix = sample(hprev, inputs[0], 200)
+            txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+            print '----\n %s \n----' % (txt, )
+        loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
+        smooth_loss = smooth_loss * 0.999 + loss * 0.001
+        if n % 100 == 0: print 'iter %d, loss: %f' % (n, smooth_loss)
+        for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],
+                                      [dWxh, dWhh, dWhy, dbh, dby],
+                                      [mWxh, mWhh, mWhy, mbh, mby]):
+                                          mem += dparam * dparam
+                                          param += -learning_rate * dparam / np.sqrt(mem + 1e-8)
+        p += seq_length
+        n += 1
